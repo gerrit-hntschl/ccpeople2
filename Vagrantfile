7b8f02392bb25853c127e0cbeeb7cc8e6bf0c83e
Gerrit Hentschel <gerrit.hentschel@codecentric.de>
2015-08-24 09:00:34 +0200

    add dev build

--- Vagrantfile
+++ Vagrantfile
@@ -147,7 +147,7 @@ Vagrant.configure("2") do |config|
       config.vm.network "forwarded_port", guest: 8000, host: 8000, auto_correct: true
       config.vm.network "forwarded_port", guest: 4334, host: 4334, auto_correct: true
       config.vm.network "forwarded_port", guest: 5432, host: 5432, auto_correct: true
-      config.vm.network "forwarded_port", guest: 35000, host: 35000, auto_correct: true
+      config.vm.network "forwarded_port", guest: 35001, host: 35001, auto_correct: true
 
 
       # Uncomment below to enable NFS for sharing the host machine into the coreos-vagrant VM.
--- app/project.clj
+++ app/project.clj
@@ -3,17 +3,21 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-alpha2"]
+  :dependencies [[org.clojure/clojure "1.8.0-alpha4"]
                  [aleph "0.4.0"]
                  [org.clojure/clojurescript "1.7.48"]
                  [prismatic/schema "0.4.3"]
                  [com.stuartsierra/component "0.2.3"]
                  [org.postgresql/postgresql "9.3-1102-jdbc41"]
                  [com.datomic/datomic-pro "0.9.5206"
-                  :exclusions [org.slf4j/slf4j-nop joda-time org.slf4j/slf4j-log4j12 org.slf4j/slf4j-api]]]
+                  :exclusions [org.slf4j/slf4j-nop
+                               joda-time org.slf4j/slf4j-log4j12
+                               org.slf4j/slf4j-api]]
+                 [ch.qos.logback/logback-classic "1.0.1"]]
   :profiles {:dev        {:source-paths ["dev"]
                           :plugins      [[lein-cljsbuild "1.0.5"]
-                                         [lein-environ "1.0.0"]]}
+                                         [lein-environ "1.0.0"]]
+                          :dependencies [[alembic "0.3.2"]]}
              :uberjar    {:aot :all
                           :uberjar-name "app.jar"
                           :omit-source true}}
