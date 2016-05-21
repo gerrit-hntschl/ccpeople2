5866fa141b48fdd5d01524d82ded039820b9fdba
Gerrit Hentschel <gerrit.hentschel@codecentric.de>
2016-05-21 10:49:22 +0200

    Re-add build and push

--- app/build.sh
+++ app/build.sh
@@ -10,5 +10,5 @@ fi
 # Compile Clojurescript and uberjar everything up
 docker run -it -v /home/core/share/app:/usr/src/app -v /home/core/.m2:/root/.m2 -e "JIRA_BASE_URL=${JIRA_BASE_URL}" app-uberjar-build
 # Create image containing app + java
-#docker build -t dockerhub.codecentric.de/ccdashboard-app -f Dockerfile-uberjar .
-#docker push dockerhub.codecentric.de/ccdashboard-app
+docker build -t dockerhub.codecentric.de/ccdashboard-app -f Dockerfile-uberjar .
+docker push dockerhub.codecentric.de/ccdashboard-app
