67b0b43d15bc393b0c400c623f75debf180838b1
x1n4u <felix.ehrenpfort@codecentric.de>
2016-06-10 14:07:09 +0200

    Change Test to be more useful * Change of Input-data * Change expected-result, now matches "real-data" * No Specific Querys, use of existing function

--- app/test/ccdashboard/test/user-and-team.clj
+++ app/test/ccdashboard/test/user-and-team.clj
@@ -1,94 +1,142 @@
 (ns ccdashboard.test.user-and-team
-  (:require [ccdashboard.ticket-import.core :refer :all]
+  (:require [schema.experimental.complete :as c]
+            [ccdashboard.ticket-import.core :refer :all]
             [datomic.api :as d]
             [ccdashboard.system :as system]
             [ccdashboard.config :as config]
+            [ccdashboard.domain.data-model :as model]
             [com.stuartsierra.component :as component]
             [ccdashboard.persistence.core :as storage]
             [clojure.test :refer [is deftest run-tests]])
   (:import (java.util UUID)))
 
-(def ^:const team-berlin  {:id 101 :name "Berlin"})
+(defn complete-non-nil [schema]
+  (fn [x]
+    (c/complete x schema)))
 
-(def ^:const team-hamburg {:id 200 :name "Hamburg"})
+(def team-name-and-id-scenario
+  {:fixture         []
+   :jira-state      {:teams-all (->> [{:id   101
+                                       :name "Berlin"}
+                                      {:id   200
+                                       :name "Hamburg"}]
+                                     (mapv (complete-non-nil model/JiraTeam)))}
+   :expected-result #{{:team/id   101
+                       :team/name "Berlin"}
+                      {:team/id   200
+                       :team/name "Hamburg"}}})
 
-(def ^:const bob-baumeister "bob.baumeister")
+(defn team-name-and-id-test-function [dbval scenario]
+  (is (= (:expected-result scenario)
+        (storage/all-team-informations dbval))))
 
-(def ^:const bob-email (str bob-baumeister "@codecentric.de"))
+(def user-to-team-scenario
+  {:fixture         [[{:db/id              (storage/people-tempid)
+                       :user/jira-username "peter.lustig"
+                       :user/email         "peter.lustig@codecentric.de"
+                       :user/display-name  "Peter Lustig"}
+                      {:db/id              (storage/people-tempid)
+                       :user/jira-username "pipi.langstrumpf"
+                       :user/email         "pipi.langstrumpf@codecentric.de"
+                       :user/display-name  "Pipi Langstrumpf"}
+                      {:db/id              (storage/people-tempid)
+                       :user/jira-username "bibi.blocksberg"
+                       :user/email         "bibi.blocksberg@codecentric.de"
+                       :user/display-name  "Bibi Blocksberg"}
+                      {:db/id              (storage/people-tempid)
+                       :user/jira-username "kaptain.blaubaer"
+                       :user/email         "kaptain.blaubaer@codecentric.de"
+                       :user/display-name  "Kaptain Blaubaer"}
+                      {:db/id              (storage/people-tempid)
+                       :user/jira-username "bob.baumeister"
+                       :user/email         "bob.baumeister@codecentric.de"
+                       :user/display-name  "Bob Baumeister"}]]
+   :jira-state      {:teams-all        (->> [{:id   101
+                                              :name "Berlin"}
+                                             {:id   200
+                                              :name "Hamburg"}]
+                                            (mapv (complete-non-nil model/JiraTeam)))
+                     :team-members-all (->> [{:member     {:name "peter.lustig"}
+                                              :membership {:teamId 101}}
+                                             {:member     {:name "pipi.langstrumpf"}
+                                              :membership {:teamId 101}}
+                                             {:member     {:name "bibi.blocksberg"}
+                                              :membership {:teamId 101}}
+                                             {:member     {:name "kaptain.blaubaer"}
+                                              :membership {:teamId 200}}
+                                             {:member     {:name "bob.baumeister"}
+                                              :membership {:teamId 200}}]
+                                            (mapv (complete-non-nil model/JiraTeamMember)))}
+   :expected-result #{{:user {:user/jira-username "peter.lustig",
+                              :user/email "peter.lustig@codecentric.de",
+                              :user/display-name "Peter Lustig",
+                              :user/team 101},
+                       :worklogs [],
+                       :tickets [],
+                       :customers []}
+                      {:user {:user/jira-username "kaptain.blaubaer",
+                              :user/email "kaptain.blaubaer@codecentric.de",
+                              :user/display-name "Kaptain Blaubaer",
+                              :user/team 200},
+                       :worklogs [],
+                       :tickets [],
+                       :customers []}
+                      {:user {:user/jira-username "pipi.langstrumpf",
+                              :user/email "pipi.langstrumpf@codecentric.de",
+                              :user/display-name "Pipi Langstrumpf",
+                              :user/team 101},
+                       :worklogs [],
+                       :tickets [],
+                       :customers []}
+                      {:user {:user/jira-username "bibi.blocksberg",
+                              :user/email "bibi.blocksberg@codecentric.de",
+                              :user/display-name "Bibi Blocksberg",
+                              :user/team 101},
+                       :worklogs [],
+                       :tickets [],
+                       :customers []}
+                      {:user {:user/jira-username "bob.baumeister",
+                              :user/email "bob.baumeister@codecentric.de",
+                              :user/display-name "Bob Baumeister",
+                              :user/team 200},
+                       :worklogs [],
+                       :tickets [],
+                       :customers []}}})
 
-(def ^:const team-member-test-list  [{:team-id (:id team-berlin)  :jira-username "peter.lustig"}
-                                     {:team-id (:id team-berlin)  :jira-username "pipi.langstrumpf"}
-                                     {:team-id (:id team-berlin)  :jira-username "bibi.blocksberg"}
-                                     {:team-id (:id team-hamburg) :jira-username "kaptain.blaubaer"}
-                                     {:team-id (:id team-hamburg) :jira-username bob-baumeister}])
-
-(defn to-datomic-test-user [member]
-  (let [jira-username (:jira-username member)]
-    {:db/id              (storage/people-tempid)
-     :user/email         (str jira-username "@codecentric.de")
-     :user/jira-username jira-username}))
-
-(defn test-team-member [member]
-  {:member      {:name (:jira-username member)}
-   :membership  {:teamId (:team-id member)}})
-
-(def team-scenario
-  {:fixture         [(mapv to-datomic-test-user team-member-test-list)]
-   :jira-state      {:teams-all        [team-berlin team-hamburg]
-                     :team-members-all (mapv test-team-member team-member-test-list)}
-   :expected-result {:entity-key :team-member
-                     :id-key     :user/team
-                     :expected   #{{}}}})
+(defn user-to-team-test-function [dbval scenario]
+  (is (= (:expected-result scenario)
+         (into #{}
+               (map (comp (partial storage/existing-user-data dbval)
+                          (partial storage/entity-id-by-username dbval)
+                          (partial :user/jira-username)))
+               (first (:fixture scenario))))))
 
 (defn new-in-memory-system [config]
   (assoc-in config [:datomic :connect-url] (format "datomic:mem://%s" (UUID/randomUUID))))
 
 (defn new-test-system [scenario]
-  (let [schedule-atom (atom [])]
-    (-> (system/new-system (new-in-memory-system config/defaults))
-        ;; don't need an http server for testing currently
-        (dissoc :http)
-        (assoc :jira-client (map->JiraFakeClient (:jira-state scenario)))
-        (assoc :scheduler (reify Scheduler
-                            (schedule [this f repeat-delay]
-                              (swap! schedule-atom conj f))))
-        (assoc :schedule-atom schedule-atom)
-        (component/system-using {:jira-importer [:conn :jira-client :scheduler]}))))
+  (-> (system/new-system (new-in-memory-system config/defaults))
+      ;; don't need an http server for testing currently
+      (dissoc :http)
+      (assoc :jira-client (map->JiraFakeClient (:jira-state scenario)))
+      (component/system-using {:jira-importer [:conn :jira-client :scheduler]})))
 
-(defn assert-result-matches [expected-spec actual]
-  (doseq [{:keys [entity-key id-key expected]} expected-spec]
-    (let [actual-ents (get actual entity-key)
-          id->actual-ent (into {} (map (juxt id-key identity)) actual-ents)]
-      (if (empty? expected)
-        (is (empty? (get actual entity-key)) "No results expected.")
-        (doseq [expected-ent expected]
-          (is (= expected-ent
-                 (select-keys (get id->actual-ent (get expected-ent id-key))
-                              (keys expected-ent)))))))))
-
-(defn test-scenario [scenario]
+(defn test-scenario [scenario test-function]
   (let [sys (component/start (new-test-system scenario))
         conn (:conn sys)
-        jira-import-data (jira-user-start-date-import {:dbval (d/db conn) :jira (:jira-client sys)})]
-    (println jira-import-data)
+        dbval (d/db conn)
+        jira-import-data (jira-user-start-date-import {:dbval dbval
+                                                       :jira  (:jira-client sys)})]
     (try
-      (doseq [tx (:fixture scenario)]
+      (doseq [tx (into (:fixture scenario)
+                       (:db-transactions jira-import-data))]
         @(d/transact conn tx))
-      (doseq [tx (:db-transactions jira-import-data)]
-        @(d/transact conn tx))
-      ;; execute the scheduled import synchronously
-      (def impres ((first @(:schedule-atom sys))))
-      ;; simulate login
-      (let [user-id (storage/create-openid-user conn
-                                                {:user/email         bob-email
-                                                 :user/display-name  "Bob Baumeister"
-                                                 :user/jira-username bob-baumeister})]
-        (def rr (storage/existing-user-data-for-user conn user-id))
-        (println rr)
-        (assert-result-matches (:expected-result scenario) rr))
+      (test-function dbval scenario)
       (finally
-        (component/stop sys)))
-    jira-import-data))
+        (component/stop sys)))))
 
-(deftest should-be-user-in-team
-  (test-scenario team-scenario))
+(deftest schould-be-in-datomic-team-name-and-id
+  (test-scenario team-name-and-id-scenario team-name-and-id-test-function))
+
+(deftest should-be-in-datomic-user-to-team
+  (test-scenario user-to-team-scenario user-to-team-test-function))
